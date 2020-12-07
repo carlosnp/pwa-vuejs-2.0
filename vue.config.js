cb4dac8745480614616c4d482ddf31990723ed6b
Carlos Nava <machinery.e@gmail.com>
2020-12-07 15:09:44 -0400

    Al fin funciono

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,18 @@ module.exports = {
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
-    workboxPluginMode: 'InjectManifest',
+    workboxPluginMode: 'GenerateSW',
     manifestCrossorigin: 'anonymous',
-    manifestPath:'./public/manifest.json',
+    manifestPath:'manifest.json',
+    iconPaths:{
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    },
   },
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/dist/'
+  ? './'
   : '/'
 }
